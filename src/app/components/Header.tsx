1b60873fd3218320834aab582b92f60b2f7473e1
techie-mohit <mohitkeshari2000@gmail.com>
2025-07-18 15:32:05 +0530

    header section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,6 +8,13 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { toggleLoginDialog } from "@/store/slice/userSlice";
 import { RootState } from "@/store/store";
 import {
@@ -18,6 +25,7 @@ import {
   HelpCircle,
   Lock,
   LogOut,
+  Menu,
   Package,
   PiggyBank,
   Search,
@@ -257,10 +265,80 @@ const Header = () => {
         </div>
       </div>
 
-
       {/* Mobile Header */}
 
-      
+      <div className="container mx-auto flex lg:hidden items-center justify-between p-4">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Menu className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-80 p-0">
+            <SheetHeader>
+              <SheetTitle className="sr-only"></SheetTitle>
+            </SheetHeader>
+            <div className="border-b p-4">
+              <Link href="/" className="flex items-center">
+                <Image
+                  src="/images/web-logo.png"
+                  alt="Desktop Logo"
+                  width={150}
+                  height={40}
+                  className="h-15 w-auto"
+                />
+              </Link>
+            </div>
+            <MenuItems className="py-2" />
+          </SheetContent>
+        </Sheet>
+
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/images/web-logo.png"
+            alt="Desktop Logo"
+            width={450}
+            height={100}
+            className="h-6 md:h-10 w-20 md:w-auto "
+          />
+        </Link>
+
+        <div className="flex flex-1 items-center justify-center max-w-xl px-4">
+          <div className="relative w-full">
+            <Input
+              type="text"
+              placeholder="Search Books..."
+              className="w-full pr-10"
+              value=""
+              onChange={() => {}}
+            />
+
+            <Button
+              size="icon"
+              variant="ghost"
+              className="absolute right-0 top-1/2 -translate-y-1/2"
+            >
+              <Search className="h-5 w-5" />
+            </Button>
+          </div>
+        </div>
+
+        <Link href="/checkout/cart">
+            <div className="relative">
+              <Button variant="ghost" className="relative">
+                <ShoppingCart className="h-8 w-8 mr-2" />
+              </Button>
+
+              {user && (
+                <span className="absolute top-2 left-5 transform translate-x-1/2 -translate-y-1/2 px-1 text-xs text-white bg-red-600 rounded-full">
+                  3
+                </span>
+              )}
+            </div>
+          </Link>
+
+        
+      </div>
     </header>
   );
 };
