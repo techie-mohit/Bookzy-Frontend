affb0a5ace7b16c120406ce4e9971293f3536800
techie-mohit <mohitkeshari2000@gmail.com>
2025-07-26 20:57:14 +0530

    paginated

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { Accordion, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { Checkbox } from "@/components/ui/checkbox";
 import { books, filters } from "@/lib/Constant";
-import { Key } from "lucide-react";
 import Link from "next/link";
 import React, { useState } from "react";
+import { formatDistanceToNow } from "date-fns";
+import BookLoader from "@/lib/BookLoader";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 const Page = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -13,9 +16,13 @@ const Page = () => {
   const [selectedType, setSelectedType] = useState<string[]>([]);
   const [sortOption, setSortOption] = useState("newest");
 
+  const bookPerPage = 6; // Number of books to display per page
+
+  const [isLoading, setIsLoading] = useState(false);
+
   const togglefilter = (section: string, item: string) => {
     const updatedFilter = (prev: string[]) => {
-      prev.includes(item) ? prev.filter((i) => i != item) : [...prev, item];
+      return prev.includes(item) ? prev.filter((i) => i != item) : [...prev, item];
     };
 
     switch (section) {
@@ -71,6 +78,28 @@ const Page = () => {
     }
   });
 
+  const totalPage = Math.ceil(sortedBooks.length/ bookPerPage);
+  const paginatedBooks = sortedBooks.slice(
+    (currentPage - 1) * bookPerPage,
+    currentPage * bookPerPage
+  );
+  const handlePageChange = (page : number)=>{
+    setCurrentPage(page);
+  }
+  const calculateDiscount = (price: number, finalPrice: number): number => {
+    if (price > finalPrice && price > 0) {
+      return Math.round(((price - finalPrice) / price) * 100);
+    }
+    return 0;
+  };
+
+  const formData = (dateString: Date) => {
+    const date = new Date(dateString);
+    return formatDistanceToNow(date, {addSuffix: true});
+  };
+
+
+
   return (
     <div className="min-h-screen bg-gray-100">
       <div className="container mx-auto px-4 py-8">
@@ -94,10 +123,60 @@ const Page = () => {
                   <AccordionTrigger className="text-lg font-semibold text-blue-500">
                     {key.charAt(0).toUpperCase() + key.slice(1)}
                   </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="space-y-2 mt-2">
+                      {
+                        values.map((value)=> (
+                          <div key={value} className="flex items-center space-x-2">
+                            <Checkbox 
+                            id={value}
+                            checked = {key === 'condition' ? selectedCondition.includes(value)
+                              : key === 'classType' ? selectedType.includes(value)
+                              : selectedCategory.includes(value)}
+
+                              onCheckedChange = {()=> togglefilter(key, value)}
+                          />
+
+                          <label htmlFor={value} className="text-sm font-medium leading-none">{value}</label>
+
+                          </div>
+                        ))
+                      }
+                    </div>
+                  </AccordionContent>
                 </AccordionItem>
               ))}
             </Accordion>
           </div>
+
+          <div className = "space-y-6">
+            {
+              isLoading ? (
+                <BookLoader/>
+              ):(
+                paginatedBooks.length ? (
+                  <>
+                    <div className="flex justify-between">
+                      <div className="mb-8 text-xl font-semibold">Buy Second Hand Books , Used Books Online In India</div>
+                        <Select value={sortOption} onValueChange={setSortOption}>
+                          <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Sort By"/>
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="newest">Newest First</SelectItem>
+                            <SelectItem value="oldest">Oldest First</SelectItem>
+                            <SelectItem value="price-low">Price: Low to High</SelectItem>
+                            <SelectItem value="price-high">Price: High to Low</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+                  </>  
+
+                ):false
+              )
+              
+            }
+        </div>
 
         </div>
       </div>
