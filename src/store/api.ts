4683bee05bbe6f261d295efae6f2837df79d7e7f
techie-mohit <mohitkeshari2000@gmail.com>
2025-08-23 20:00:24 +0530

    add backend url

--- src/store/api.ts
+++ src/store/api.ts
@@ -3,7 +3,49 @@
 // 2️⃣ fetchBaseQuery - ek built-in baseQuery function jo simple fetch request banata hai
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URLS = {
+
+    // user related urls 
+    REGISTER : `${BASE_URL}/auth/register`,
+    LOGIN : `${BASE_URL}/auth/login`,
+    VERIFY_EMAIL :(token:string)=> `${BASE_URL}/auth/verify-email/${token}`,
+    FORGOT_PASSWORD : `${BASE_URL}/auth/forgot-password`,
+    RESET_PASSWORD : (token:string)=> `${BASE_URL}/auth/reset-password/${token}`,
+    VERIFY_AUTH : (token:string)=> `${BASE_URL}/auth/verify`,
+    LOGOUT : `${BASE_URL}/auth/logout`,
+    UPDATE_USER_PROFILE : (userId:string)=> `${BASE_URL}/user/profile/update/${userId}`,
+
+    // product related urls
+    PRODUCTS: `${BASE_URL}/products`,
+    PRODUCT_BY_ID: (id:string)=> `${BASE_URL}/products/${id}`,
+    GET_PRODUCT_BY_SELLER_ID : (sellerId:string)=> `${BASE_URL}/products/seller/${sellerId}`,
+    DELETE_PRODUCT_BY_SELLER_ID : (productId:string)=> `${BASE_URL}/products/seller/${productId}`,
+
+
+    // cart related urls
+    CART:(userId: string)=> `${BASE_URL}/cart/${userId}`,
+    ADD_TO_CART: `${BASE_URL}/cart/add-to-cart`,
+    REMOVE_FROM_CART: (productId:string)=> `${BASE_URL}/cart/remove-from-cart/${productId}`,
+
+
+    // wishlist related urls
+    WISHLIST: (userId: string) => `${BASE_URL}/wishlist/${userId}`,
+    ADD_TO_WISHLIST: `${BASE_URL}/wishlist/add-to-wishlist`,
+    REMOVE_FROM_WISHLIST: (productId: string) => `${BASE_URL}/wishlist/remove-from-wishlist/${productId}`,
+
+
+    // order related urls
+    ORDERS: `${BASE_URL}/order`,
+    ORDER_BY_ID: (orderId: string) => `${BASE_URL}/order/${orderId}`,
+    CREATE_RAZORPAY_PAYMENT : `${BASE_URL}/order/payment-razorpay`,
+
+    // address related urls
+    ADD_ADDRESS: `${BASE_URL}/user/address/create-or-update`,
+    GET_ADDRESS: `${BASE_URL}/user/address`
+
+
+}
 
 
 // Ye Redux store mein ek RTK Query API slice banata hai
