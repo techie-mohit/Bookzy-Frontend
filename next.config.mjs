f88098a0032de836666cf05bd4953d0c06d52ee2
techie-mohit <mohitkeshari2000@gmail.com>
2025-09-26 20:06:48 +0530

    update node_modules

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import("next").NextConfig} */
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   images: {
     domains:['images.unsplash.com','media.istockphoto.com', 'cdn.pixabay.com', 'images.pexels.com', 'res.cloudinary.com'],
   }
